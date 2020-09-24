e8d9960362fa044e111916d80f3f3dcac3fcace2
Александр Журавлев <zsanek265@mail.ru>
2020-09-24 11:28:15 +0300

    ready

---
+++ 1 лаба/Laba/FormTep.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsTep
+{
+    partial class FormTep
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTep));
+            this.buttonCreateTep = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.pictureBoxTep = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTep)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreateTep
+            // 
+            this.buttonCreateTep.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCreateTep.Location = new System.Drawing.Point(11, 11);
+            this.buttonCreateTep.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCreateTep.Name = "buttonCreateTep";
+            this.buttonCreateTep.Size = new System.Drawing.Size(79, 22);
+            this.buttonCreateTep.TabIndex = 1;
+            this.buttonCreateTep.Text = "Создать";
+            this.buttonCreateTep.UseVisualStyleBackColor = true;
+            this.buttonCreateTep.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDown.Location = new System.Drawing.Point(737, 420);
+            this.buttonDown.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(30, 30);
+            this.buttonDown.TabIndex = 5;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUp.Location = new System.Drawing.Point(737, 386);
+            this.buttonUp.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 30);
+            this.buttonUp.TabIndex = 4;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRight.Location = new System.Drawing.Point(771, 420);
+            this.buttonRight.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(30, 30);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLeft.Location = new System.Drawing.Point(703, 420);
+            this.buttonLeft.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(30, 30);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxTep
+            // 
+            this.pictureBoxTep.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxTep.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxTep.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxTep.Name = "pictureBoxTep";
+            this.pictureBoxTep.Size = new System.Drawing.Size(822, 461);
+            this.pictureBoxTep.TabIndex = 0;
+            this.pictureBoxTep.TabStop = false;
+            this.pictureBoxTep.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // FormTep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 461);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonCreateTep);
+            this.Controls.Add(this.pictureBoxTep);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormTep";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Тепловоз";
+            this.TransparencyKey = System.Drawing.Color.White;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTep)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonCreateTep;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.PictureBox pictureBoxTep;
+    }
+}
+
---
+++ 1 лаба/Laba/FormTep.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTep
+{
+    public partial class FormTep : Form
+    {
+        private Teplovoz tep;
+
+        public FormTep()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxTep.Width, pictureBoxTep.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            
+            tep.DrawTep(gr);
+            pictureBoxTep.Image = bmp;
+        }
+        
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+           
+            tep = new Teplovoz(rnd.Next(70, 300), rnd.Next(1000, 2000), Color.ForestGreen,
+           Color.Gray, true, true);
+            tep.SetPosition(rnd.Next(40, 600), rnd.Next(10, 100), pictureBoxTep.Width,
+           pictureBoxTep.Height);
+            
+            Draw();
+        }
+       
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    tep.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    tep.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    tep.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    tep.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        private void pictureBoxLoc_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
